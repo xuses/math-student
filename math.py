769e6a7d77328c23312386df2b5760ea7ab64bd5
xuses <33555129+xuses@users.noreply.github.com>
2022-11-24 18:49:21 +0700

    Update math.py

--- math.py
+++ math.py
@@ -58,7 +58,7 @@ clf.fit(X_train,y_train)
 pred = clf.predict(X_test)
 print(classification_report(y_test,pred))
 print(accuracy_score(y_test,pred)) #accuracy
-a = [[sex,etnik,pend_ortu,maksi,kursus,baca,tulis]] #women,group d,associate's degree, standard lunch, none preparation, 60 reading score, 90 writing score
+a = [sex,etnik,pend_ortu,maksi,kursus,baca,tulis] #women,group d,associate's degree, standard lunch, none preparation, 60 reading score, 90 writing score
 a = s.transform(a)
 b = clf.predict(a)
 b
